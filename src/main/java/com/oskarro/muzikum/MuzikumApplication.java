66a013c576cb3c5ae6129152b449e5b4138b1dbb
oskarro <oskar.slyk@gmail.com>
2020-03-12 22:09:32 +0100

    first view

---
+++ src/main/java/com/oskarro/muzikum/MuzikumApplication.java
@@ -0,0 +1,13 @@
+package com.oskarro.muzikum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuzikumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuzikumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oskarro/muzikum/MuzikumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oskarro.muzikum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuzikumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
