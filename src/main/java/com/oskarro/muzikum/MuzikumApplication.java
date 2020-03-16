ab34b13598f435e9277a86daf0a3c0218dcb8388
oskarro <oskar.slyk@gmail.com>
2020-03-16 21:14:02 +0100

    init

--- src/main/java/com/oskarro/muzikum/MuzikumApplication.java
+++ src/main/java/com/oskarro/muzikum/MuzikumApplication.java
@@ -15,6 +15,7 @@ public class MuzikumApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MuzikumApplication.class, args);
+
     }
 
 }
--- src/main/java/com/oskarro/muzikum/track/TrackController.java
+++ src/main/java/com/oskarro/muzikum/track/TrackController.java
@@ -1,31 +1,29 @@
 package com.oskarro.muzikum.track;
 
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping(value = "/tracks")
 public class TrackController {
 
     private final TrackService trackService;
 
-    public TrackController(TrackService trackService) {
-        this.trackService = trackService;
-    }
+    private TrackRepository trackRepository;
 
-    //@GetMapping
-    public List<Track> getTracks() {
-        List<Track> trackList = new ArrayList<>();
-        Track track = Track.builder().title("aaa").artist("bbb").genre("club").points(31).id(1).build();
-        Track track1 = Track.builder().title("aa2a").artist("b32bb").genre("dance").points(11).id(2).build();
-        Track track2 = Track.builder().title("aa34a").artist("bb34b").genre("club").points(55).id(3).build();
-        trackList.add(track);
-        trackList.add(track1);
-        trackList.add(track2);
-        return trackList;
+    @GetMapping(value = "initTracks")
+    public String getTracks() {
+        trackRepository.saveAll(Arrays.asList(
+                Track.builder().title("aaa").artist("bbb").genre("club").points(31).id(1).build(),
+                Track.builder().title("aa2a").artist("b32bb").genre("dance").points(11).id(2).build(),
+                Track.builder().title("aa34a").artist("bb34b").genre("club").points(55).id(3).build()));
+        return "All tracks have been added!";
     }
 
     @GetMapping
