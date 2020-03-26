2b2dfa57efa93d6d3d202d58b3ace681d665eb75
oskarro <oskar.slyk@gmail.com>
2020-03-26 20:00:22 +0100

    MusicList crawler added

---
+++ server/src/main/java/com/oskarro/muzikum/provider/contractor/MusicListServiceImpl.java
@@ -0,0 +1,20 @@
+package com.oskarro.muzikum.provider.contractor;
+
+import com.oskarro.muzikum.provider.Provider;
+import com.oskarro.muzikum.track.Genre;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MusicListServiceImpl implements MusicListService {
+
+
+    @Override
+    public String getTrackList(Provider provider) {
+        return null;
+    }
+
+    @Override
+    public String getTracklistByGenre(Provider provider, Genre genre) {
+        return null;
+    }
+}
