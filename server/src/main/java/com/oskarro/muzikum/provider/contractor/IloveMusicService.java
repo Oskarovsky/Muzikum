013717bdf67e5952f2b95aa59284c5527928a5e2
oskarro <oskar.slyk@gmail.com>
2020-03-28 10:58:12 +0100

    keeping tips

---
+++ server/src/main/java/com/oskarro/muzikum/provider/contractor/IloveMusicService.java
@@ -0,0 +1,7 @@
+package com.oskarro.muzikum.provider.contractor;
+
+public interface IloveMusicService {
+
+    // TODO implementation for ilovemusic.de
+
+}
---
+++ server/src/main/java/com/oskarro/muzikum/provider/contractor/IloveMusicServiceImpl.java
@@ -0,0 +1,11 @@
+package com.oskarro.muzikum.provider.contractor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class IloveMusicServiceImpl implements IloveMusicService {
+
+    // TODO implementation for ilovemusic.de
+}
