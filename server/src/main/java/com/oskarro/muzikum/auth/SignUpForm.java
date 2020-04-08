40a19119be16bab0a3bdd56fe033227425f27385
oskarro <oskar.slyk@gmail.com>
2020-04-08 20:50:32 +0200

    full forms

---
+++ server/src/main/java/com/oskarro/muzikum/auth/SignUpForm.java
@@ -0,0 +1,32 @@
+package com.oskarro.muzikum.auth;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+import javax.validation.constraints.*;
+
+@Getter
+@Setter
+public class SignUpForm {
+    @NotBlank
+    @Size(min = 3, max = 50)
+    private String name;
+
+    @NotBlank
+    @Size(min = 3, max = 50)
+    private String username;
+
+    @NotBlank
+    @Size(max = 60)
+    @Email
+    private String email;
+
+    private Set<String> role;
+
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String password;
+
+}
---
+++ server/src/main/java/com/oskarro/muzikum/user/Role.java
@@ -0,0 +1,25 @@
+package com.oskarro.muzikum.user;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.NaturalId;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @NaturalId
+    @Column(length = 60)
+    private RoleName name;
+}
