ca1346ba8be2619bed8fcd106f83b9934de1dccb
oskarro <oskar.slyk@gmail.com>
2020-04-10 00:37:26 +0200

    video API for gut

---
+++ frontend/src/app/services/video/video.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Track } from '../../track/model/track';
+import {Observable} from 'rxjs';
+import {Playlist} from '../../playlist/model/playlist';
+import {Video} from '../../video/model/video';
+
+@Injectable({providedIn: 'root'})
+export class VideoService {
+
+  public API = '//localhost:8080/api';
+  // public API = '//91.205.75.145:8080/api';
+  public VIDEO_API = this.API + '/video';
+
+  tracks: Track[] = [];
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(private http: HttpClient) { }
+
+  /** GET all video */
+  getAllVideos(): Observable<Video[]> {
+    return this.http.get<Video[]>(this.VIDEO_API + '/findAll');
+  }
+
+  /** GET video by id */
+  getVideo(id: string): Observable<any> {
+    return this.http.get<Video>(this.VIDEO_API + '/' + id);
+  }
+
+  addVideo(video: Video): Observable<Video> {
+    return this.http.post<Video>(this.VIDEO_API  + '/add', video);
+  }
+
+  deleteVideo(id: number): Observable<any> {
+    return this.http.delete<Video>(this.VIDEO_API + '/' + id);
+  }
+
+}
