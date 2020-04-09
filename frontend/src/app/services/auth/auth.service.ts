e9422e4f11714fb906cfdbe3df8db2f7f06b5551
oskarro <oskar.slyk@gmail.com>
2020-04-09 19:48:28 +0200

    service init

--- frontend/src/app/services/auth/auth.service.ts
+++ frontend/src/app/services/auth/auth.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const API = '//localhost:8080/api';
+// public API = '//91.205.75.145:8080/api';
+const AUTH_API = this.API + '/auth';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  login(credentials): Observable<any> {
+    return this.httpClient.post(AUTH_API + '/signin', {
+      username: credentials.username,
+      password: credentials.password
+    }, httpOptions);
+  }
+
+  register(user): Observable<any> {
+    return this.httpClient.post(AUTH_API + '/signup', {
+      username: user.username,
+      email: user.email,
+      password: user.password
+    }, httpOptions);
+  }
+
 }
