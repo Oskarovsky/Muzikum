38bb2a68cd5a81b4ee4aaeb25295fed32b387e08
oskarro <oskar.slyk@gmail.com>
2020-04-18 15:20:15 +0200

    models service

---
+++ server/src/main/java/com/oskarro/muzikum/storage/FilesStorageService.java
@@ -0,0 +1,20 @@
+package com.oskarro.muzikum.storage;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+public interface FilesStorageService {
+
+    void init();
+
+    void save(MultipartFile file);
+
+    Resource load(String filename);
+
+    void deleteAll();
+
+    Stream<Path> loadAll();
+}
---
+++ server/src/main/java/com/oskarro/muzikum/storage/FilesStorageServiceImpl.java
@@ -0,0 +1,69 @@
+package com.oskarro.muzikum.storage;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.util.FileSystemUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+@Service
+public class FilesStorageServiceImpl implements FilesStorageService {
+
+    private final Path rootPath = Paths.get("uploads");
+
+    @Override
+    public void init() {
+        try {
+            Files.createDirectory(rootPath);
+        } catch (IOException e) {
+            throw new RuntimeException("Could not initialize folder for upload!");
+        }
+    }
+
+    @Override
+    public void save(MultipartFile file) {
+        try {
+            Files.copy(file.getInputStream(), this.rootPath.resolve(Objects.requireNonNull(file.getOriginalFilename())));
+        } catch (Exception e) {
+            throw new RuntimeException("Could not store the file. Error: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public Resource load(String filename) {
+        try {
+            Path file = rootPath.resolve(filename);
+            Resource resource = new UrlResource(file.toUri());
+
+            if (resource.exists() || resource.isReadable()) {
+                return resource;
+            } else {
+                throw new RuntimeException("Could not read the file!");
+            }
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Error: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void deleteAll() {
+        FileSystemUtils.deleteRecursively(rootPath.toFile());
+    }
+
+    @Override
+    public Stream<Path> loadAll() {
+        try {
+            return Files.walk(this.rootPath, 1).filter(path -> !path.equals(this.rootPath)).map(this.rootPath::relativize);
+        } catch (IOException e) {
+            throw new RuntimeException("Could not load the files!");
+        }
+    }
+}
