ad53ce947dbd6af507895eb3eaa3cfe5ef8387ee
oskarro <oskar.slyk@gmail.com>
2020-04-29 17:23:48 +0200

    first step

--- server/src/main/java/com/oskarro/muzikum/user/UserRepository.java
+++ server/src/main/java/com/oskarro/muzikum/user/UserRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.swing.text.html.Option;
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
@@ -11,6 +13,10 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
 
     Optional<User> findByUsername(String username);
+
+    Optional<User> findByUsernameOrEmail(String username, String email);
+
+    List<User> findByIdIn(Collection<Integer> id);
 
     Boolean existsByUsername(String username);
 
