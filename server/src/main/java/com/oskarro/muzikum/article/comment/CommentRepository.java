8f02c58298c32eee6f42238b448ade005c016281
oskarro <oskar.slyk@gmail.com>
2020-05-12 20:20:41 +0200

    post controller and post and comments repo

--- server/src/main/java/com/oskarro/muzikum/article/comment/CommentRepository.java
+++ server/src/main/java/com/oskarro/muzikum/article/comment/CommentRepository.java
@@ -1,4 +1,16 @@
 package com.oskarro.muzikum.article.comment;
 
-public class CommentRepository {
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Integer> {
+
+    Page<Comment> findByPostId(Integer postId, Pageable pageable);
+
+    Optional<Comment> findByIdAndPostId(Integer id, Integer postId);
 }
--- server/src/main/java/com/oskarro/muzikum/article/post/PostController.java
+++ server/src/main/java/com/oskarro/muzikum/article/post/PostController.java
@@ -1,4 +1,49 @@
 package com.oskarro.muzikum.article.post;
 
+import com.oskarro.muzikum.exception.ResourceNotFoundException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
 public class PostController {
+
+    private PostRepository postRepository;
+
+    public PostController(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    @GetMapping(value = "/posts")
+    public Page<Post> getAllPosts(Pageable pageable) {
+        return postRepository.findAll(pageable);
+    }
+
+    @PostMapping(value = "/posts")
+    public Post createPost(@Valid @RequestBody Post post) {
+        return postRepository.save(post);
+    }
+
+    @PutMapping(value = "/posts/{postId}")
+    public Post updatePost(@PathVariable Integer postId, @Valid @RequestBody Post postRequest) {
+        return postRepository.findById(postId)
+                .map(post -> {
+                    post.setTitle(postRequest.getTitle());
+                    post.setDescription(postRequest.getDescription());
+                    post.setContent(postRequest.getContent());
+                    return postRepository.save(post);
+                }).orElseThrow(() -> new ResourceNotFoundException("Post", "id", postId));
+    }
+
+    @DeleteMapping(value = "/posts/{postId}")
+    public ResponseEntity<?> deletePost(@PathVariable Integer postId) {
+        return postRepository.findById(postId)
+                .map(post -> {
+                    postRepository.delete(post);
+                    return ResponseEntity.ok().build();
+                }).orElseThrow(() -> new ResourceNotFoundException("Post", "id", postId));
+    }
 }
