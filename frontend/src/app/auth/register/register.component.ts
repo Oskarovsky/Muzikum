ee1ef6aa8c5e690302b8f95ca696b4c80d2f2167
oskarro <oskar.slyk@gmail.com>
2020-08-14 17:22:27 +0200

    register panel

--- frontend/src/app/auth/register/register.component.ts
+++ frontend/src/app/auth/register/register.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth/auth.service';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators, FormControl} from '@angular/forms';
 import {MustMatch} from './MustMatch';
 
 @Component({
