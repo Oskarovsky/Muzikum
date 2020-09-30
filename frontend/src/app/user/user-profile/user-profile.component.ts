a54f04bb9281d1bf76c4f70ff731fa473b7b57df
oskarro <oskar.slyk@gmail.com>
2020-09-30 20:41:26 +0200

    modyfing user profile

--- frontend/src/app/user/user-profile/user-profile.component.ts
+++ frontend/src/app/user/user-profile/user-profile.component.ts
@@ -4,6 +4,9 @@ import {TokenStorageService} from '../../services/auth/token-storage.service';
 import {UploadFileService} from '../../services/storage/upload-file.service';
 import {FavoriteService} from '../../services/favorite/favorite.service';
 import {TrackService} from '../../services/track/track.service';
+import {Subscription} from 'rxjs';
+import {ActivatedRoute} from '@angular/router';
+import {UserService} from '../../services/user/user.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -13,23 +16,44 @@ import {TrackService} from '../../services/track/track.service';
 export class UserProfileComponent implements OnInit {
 
   currentUser: any;
+  sub: Subscription;
   message = '';
   userAvatar: any;
   imageToShow: any;
   isImageLoading: any;
   tracks: Array<any>;
   favoriteTracksByUser: Track[] = [];
+  userProfile: any;
 
   constructor(private token: TokenStorageService,
               private uploadService: UploadFileService,
               private favoriteService: FavoriteService,
+              private route: ActivatedRoute,
+              private userService: UserService,
               private trackService: TrackService) { }
 
   ngOnInit() {
     this.currentUser = this.token.getUser();
+    this.getUserProfile();
     this.getImageFromService();
-    this.getLastAddedTracksByUsername(this.currentUser.username, 5);
-    this.getAllFavoritesTracksByUser(this.currentUser.username);
+    this.getLastAddedTracksByUsername(this.userProfile.username, 5);
+    this.getAllFavoritesTracksByUser(this.userProfile.username);
+  }
+
+  getUserProfile() {
+    this.sub = this.route.params.subscribe(params => {
+      const username = params.username;
+      if (username) {
+        this.userService.getUserByUsername(username).subscribe(
+          response => {
+            this.userProfile = response;
+          },
+          error => {
+            alert('An error with fetching user by username has occurred');
+          }
+        );
+      }
+    });
   }
 
   createImageFromBlob(image: Blob) {
