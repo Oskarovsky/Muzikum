6a950bd7a403ccbda4ef690a81d387919e13a99b
oskarro <oskar.slyk@gmail.com>
2020-10-28 23:26:10 +0100

    Saving user via FB

--- server/src/main/java/com/oskarro/muzikum/security/oauth2/CustomOAuth2UserService.java
+++ server/src/main/java/com/oskarro/muzikum/security/oauth2/CustomOAuth2UserService.java
@@ -1,2 +1,100 @@
-package com.oskarro.muzikum.security.oauth2;public class CustomOAuth2UserService {
+package com.oskarro.muzikum.security.oauth2;
+
+import com.oskarro.muzikum.exception.OAuth2AuthenticationProcessingException;
+import com.oskarro.muzikum.user.AuthProvider;
+import com.oskarro.muzikum.user.User;
+import com.oskarro.muzikum.user.UserPrincipal;
+import com.oskarro.muzikum.user.UserRepository;
+import com.oskarro.muzikum.user.role.Role;
+import com.oskarro.muzikum.user.role.RoleName;
+import com.oskarro.muzikum.user.role.RoleRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+import org.springframework.security.core.AuthenticationException;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Optional;
+
+/**
+ * The CustomOAuth2UserService extends Spring Security’s DefaultOAuth2UserService and implements its loadUser() method.
+ * This method is called after an access token is obtained from the OAuth2 provider.
+ *
+ * In this method, we first fetch the user’s details from the OAuth2 provider.
+ * If a user with the same email already exists in our database then we update his details, otherwise, we register a new user.
+ * */
+
+@Slf4j
+@Service
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest request) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(request);
+        try {
+            return processOAuth2User(request, oAuth2User);
+        } catch (AuthenticationException ex) {
+            throw ex;
+        } catch (Exception ex) {
+            // Throwing an instance of AuthenticationException will trigger the OAuth2AuthenticationFailureHandler
+            throw new InternalAuthenticationServiceException(ex.getMessage(), ex.getCause());
+
+        }
+    }
+
+    private OAuth2User processOAuth2User(OAuth2UserRequest request, OAuth2User oAuth2User) {
+        OAuth2UserInfo oAuth2UserInfo = OAuth2UserInfoFactory
+                .getOAuth2UserInfo(request.getClientRegistration().getRegistrationId(), oAuth2User.getAttributes());
+
+        Optional<User> userOptional = userRepository.findByEmail(oAuth2UserInfo.getEmail());
+        User user;
+        if (userOptional.isPresent()) {
+            user = userOptional.get();
+            if (!user.getProvider().equals(AuthProvider.valueOf(request.getClientRegistration().getRegistrationId()))) {
+                throw new OAuth2AuthenticationProcessingException("Looks like you're signed up with " +
+                        user.getProvider() + " account. Please use your " + user.getProvider() + " account to login.");
+            }
+            user = updateExistingUser(user, oAuth2UserInfo);
+        } else {
+            user = registerNewUser(request, oAuth2UserInfo);
+        }
+        return UserPrincipal.create(user, oAuth2User.getAttributes());
+    }
+
+    private User registerNewUser(OAuth2UserRequest request, OAuth2UserInfo oAuth2UserInfo) {
+
+        Role roleUser = roleRepository.findByName(RoleName.ROLE_USER).orElse(null);
+
+        User user = User.builder()
+                .username(oAuth2UserInfo.getUsername())
+                .email(oAuth2UserInfo.getEmail())
+                .password("111111")
+                .roles(new HashSet(Collections.singletonList(roleUser)))
+                .provider(AuthProvider.facebook)
+                .imageUrl(oAuth2UserInfo.getImageUrl())
+                .build();
+
+        System.out.println(user);
+        return userRepository.save(user);
+    }
+
+    private User updateExistingUser(User existingUser, OAuth2UserInfo oAuth2UserInfo) {
+        existingUser.setUsername(oAuth2UserInfo.getUsername());
+        existingUser.setImageUrl(oAuth2UserInfo.getImageUrl());
+        return userRepository.save(existingUser);
+    }
+
 }
