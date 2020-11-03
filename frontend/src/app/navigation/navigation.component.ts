86ccc8f73df06a035e33f0fdcf89d8e050d6f9ba
oskarro <oskar.slyk@gmail.com>
2020-11-03 20:19:15 +0100

    adding new nav list

--- frontend/src/app/navigation/navigation.component.ts
+++ frontend/src/app/navigation/navigation.component.ts
@@ -53,11 +53,6 @@ export class NavigationComponent implements OnInit, AfterViewInit {
       route: '/profile',
     },
     {
-      displayName: 'Wyloguj się',
-      iconName: 'slideshow',
-      route: '',
-    },
-    {
       displayName: 'Utwory',
       iconName: 'slideshow',
       route: '',
@@ -108,6 +103,26 @@ export class NavigationComponent implements OnInit, AfterViewInit {
           displayName: 'Club',
           iconName: 'slideshow',
           route: '/tracks/club',
+        },
+        {
+          displayName: 'Dance',
+          iconName: 'slideshow',
+          route: '/tracks/dance',
+        },
+        {
+          displayName: 'Retro',
+          iconName: 'slideshow',
+          route: '/tracks/retro',
+        },
+        {
+          displayName: 'House',
+          iconName: 'slideshow',
+          route: '/tracks/house',
+        },
+        {
+          displayName: 'Techno',
+          iconName: 'slideshow',
+          route: '/tracks/techno',
         }
       ]
     },
