f3825c9bd02a5e44a581ab4f09a66f647b3a8f92
oskarro <oskar.slyk@gmail.com>
2020-11-07 12:17:33 +0100

    test

--- server/src/main/generated/com/oskarro/muzikum/mapper/TrackMapperImpl.java
+++ server/src/main/generated/com/oskarro/muzikum/mapper/TrackMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-11-05T21:05:21+0100",
+    date = "2020-11-07T11:42:18+0100",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 1.8.0_272 (Private Build)"
 )
 @Component
---
+++ server/src/test/java/com/oskarro/muzikum/repository/NPlusOneQueriesLoggingTest.java
@@ -0,0 +1,43 @@
+package com.oskarro.muzikum.repository;
+
+import com.oskarro.muzikum.track.TrackCommentRepository;
+import com.oskarro.muzikum.track.TrackRepository;
+import com.oskarro.muzikum.track.model.Track;
+import com.oskarro.muzikum.track.model.TrackComment;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest("hibernate.query.interceptor.error-level=ERROR")
+@Transactional
+public class NPlusOneQueriesLoggingTest {
+
+    @Autowired
+    TrackRepository trackRepository;
+
+    @Autowired
+    TrackCommentRepository trackCommentRepository;
+
+    // Fetch the tracks without the users
+    @Test
+    public void nPlusOneQueriesDetection_isLoggingWhenDetectingNPlusOneQueries() {
+        List<Track> tracks = trackRepository.findAll();
+
+        List<String> names = tracks.stream()
+                .filter(mes -> mes.getProvider()!=null)
+                .map(message -> message.getTitle() + " " + message.getProvider().getName())
+                .collect(Collectors.toList());
+
+        List<TrackComment> comments = trackCommentRepository.findAll();
+        List<String> names2 = comments.stream()
+                .map(message -> message.getTrack().getTitle())
+                .collect(Collectors.toList());
+    }
+}
