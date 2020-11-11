35c54baa3935964ca51986638defdd59e065cb72
oskarro <oskar.slyk@gmail.com>
2020-11-11 15:56:35 +0100

    minor fixes

--- server/src/main/java/com/oskarro/muzikum/voting/Vote.java
+++ server/src/main/java/com/oskarro/muzikum/voting/Vote.java
@@ -15,7 +15,7 @@ import javax.persistence.*;
 public class Vote {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private VoteType voteType;
