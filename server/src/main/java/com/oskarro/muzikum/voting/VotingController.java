f83b7ebbcc111238410751542542f29ff1c5a505
oskarro <oskar.slyk@gmail.com>
2020-11-25 20:51:47 +0100

    ResponseEntity for voting

--- server/src/main/java/com/oskarro/muzikum/voting/VotingController.java
+++ server/src/main/java/com/oskarro/muzikum/voting/VotingController.java
@@ -6,6 +6,8 @@ import com.oskarro.muzikum.track.model.Track;
 import com.oskarro.muzikum.track.TrackService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.xml.bind.ValidationException;
@@ -25,26 +27,29 @@ public class VotingController {
 
     @GetMapping(value = "/track/{trackId}")
     @CrossOrigin(origins = "*", allowedHeaders = "*")
-    public List<Vote> getVotesByTrackId(@PathVariable Integer trackId) {
-        return votingRepository.findVotesByTrackId(trackId);
+    public ResponseEntity<List<Vote>> getVotesByTrackId(@PathVariable Integer trackId) {
+        return new ResponseEntity<>(votingRepository.findVotesByTrackId(trackId), HttpStatus.OK);
     }
 
     @GetMapping(value = "/track/{trackId}/all")
     @CrossOrigin(origins = "*", allowedHeaders = "*")
-    public Integer getNumberOfVotesByTrackId(@PathVariable Integer trackId) {
-        return votingService.getNumberOfVotesByTrackId(trackId);
+    public ResponseEntity<Integer> getNumberOfVotesByTrackId(@PathVariable Integer trackId) {
+        return new ResponseEntity<>(votingService.getNumberOfVotesByTrackId(trackId), HttpStatus.OK);
     }
 
     @GetMapping(value = "/track/{trackId}/add/{username}")
     @CrossOrigin(origins = "*", allowedHeaders = "*")
-    public void addVoteForTrackById(@PathVariable Integer trackId, @PathVariable String username) {
+    public ResponseEntity<Void> addVoteForTrackById(@PathVariable Integer trackId, @PathVariable String username) {
         votingService.addVoteForTrack(trackId, username);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PostMapping(value = "/track")
     @CrossOrigin(origins = "*", allowedHeaders = "*")
-    public void voteForTrackById(@RequestBody VoteDto vote) throws ValidationException {
+    public ResponseEntity<Void> voteForTrackById(@RequestBody VoteDto vote) throws ValidationException {
         votingService.voteForTrack(vote);
+        return new ResponseEntity<>(HttpStatus.OK);
+
     }
 
     @GetMapping(value = "/{username}/tracks")
