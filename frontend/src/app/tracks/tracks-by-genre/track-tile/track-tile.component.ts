d6639b297c1e1fecbe2227e7caf645b39db9b49f
oskarro <oskar.slyk@gmail.com>
2020-12-03 23:50:07 +0100

    lazy loading for player

--- frontend/src/app/tracks/tracks-by-genre/track-tile/track-tile.component.ts
+++ frontend/src/app/tracks/tracks-by-genre/track-tile/track-tile.component.ts
@@ -1,5 +1,6 @@
 import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 import {Track} from '../../track/model/track';
+import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-track-tile',
@@ -8,17 +9,27 @@ import {Track} from '../../track/model/track';
 })
 export class TrackTileComponent implements OnInit {
 
+
   @Input() tracks: Array<Track>;
   @Input() totalNumberOfTracks: number;
   @Input() totalNumberOfPages: number;
   @Input() currentPage: number;
   @Input() isLoggedIn: boolean;
 
-  element: ElementRef;
+  mapa: Map<number, SafeResourceUrl> = new Map<number, SafeResourceUrl>();
+  track: Track;
+  url = 'https://i.ibb.co/JrDVPRN/Untitled.png';
+  urlSafe: SafeResourceUrl;
 
-  constructor() {}
+  constructor(public sanitizer: DomSanitizer) {}
+
+  fakeClick(trackId: number) {
+    this.track = this.tracks.find(x => x.id === trackId);
+    this.mapa.set(this.track.id, this.track.safeUrl);
+  }
 
   ngOnInit() {
+    this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
   }
 
 }
