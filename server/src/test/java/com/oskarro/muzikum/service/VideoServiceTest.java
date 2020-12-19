fac347c2caf4e3fb084e4b059c9cc54637b8a782
oskarro <oskar.slyk@gmail.com>
2020-12-19 23:41:04 +0100

    full test passed

--- server/src/test/java/com/oskarro/muzikum/service/VideoServiceTest.java
+++ server/src/test/java/com/oskarro/muzikum/service/VideoServiceTest.java
@@ -5,6 +5,7 @@ import com.oskarro.muzikum.video.Video;
 import com.oskarro.muzikum.video.VideoRepository;
 import com.oskarro.muzikum.video.VideoService;
 import org.apache.commons.io.IOUtils;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -30,21 +31,29 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 //@Ignore
 public class VideoServiceTest {
 
-//    @Autowired
-//    VideoService videoService;
-//
-//    @Autowired
-//    VideoRepository videoRepository;
+    @Autowired
+    VideoService videoService;
+
+    @Autowired
+    VideoRepository videoRepository;
 
     @Test
     public void test_updateYoutubeDetailsOneVideo() throws IOException, ParseException {
         String videoUrl = "moFuKK_Ac";
-        Object object = new JSONParser().parse(IOUtils.toString(new URL(videoUrl), UTF_8));
+        String basicUrl = "https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=WRooj5n80uo&key=AIzaSyDeSTKXmaye9ula3yzeA4BJWO1vA8_i8kE";
+        Object object = new JSONParser().parse(IOUtils.toString(new URL(basicUrl), UTF_8));
         JSONObject jsonObject = (JSONObject) object;
-        Integer val1 = (Integer) jsonObject.get("statistics.viewCount");
-/*        Video video = videoRepository.findByUrl(videoUrl)
+        JSONArray val1 = (JSONArray) jsonObject.get("items");
+
+        JSONObject resultObject = (JSONObject) val1.get(0);
+        String val2 = String.valueOf(resultObject.get("statistics"));
+
+        JSONObject resultObject2 = (JSONObject) resultObject.get("statistics");
+        String val3 = String.valueOf(resultObject2.get("viewCount"));
+
+        System.out.println(val3);
+        Video video = videoRepository.findByUrl(videoUrl)
                 .orElseThrow(() -> new ResourceNotFoundException("Video", "videoUrl", videoUrl));
-        video.setViewCount(val1);
-        videoRepository.save(video);*/
+        video.setViewCount(Integer.valueOf(val3));
     }
 }
