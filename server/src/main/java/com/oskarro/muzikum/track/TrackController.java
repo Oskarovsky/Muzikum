7757680f8b1b8bf9b46564fd1c7d456284a872d0
Oskarovsky <oskar.slyk@gmail.com>
2021-12-31 15:20:02 +0100

    Toplist controller fix

--- server/src/main/java/com/oskarro/muzikum/track/TrackController.java
+++ server/src/main/java/com/oskarro/muzikum/track/TrackController.java
@@ -75,20 +75,6 @@ public class TrackController {
         return trackService.findTracksByGenre(genre);
     }
 
-    @PostMapping(value = "/addToRanking")
-    @CrossOrigin(origins = "*", allowedHeaders = "*")
-    void addTrackToRanking (@RequestBody Track track, BindingResult bindingResult) throws ValidationException {
-        if (bindingResult.hasErrors()) {
-            throw new ValidationException("Track has errors - it cannot by send");
-        }
-        Track trackAdded = Track.builder()
-                .artist(track.getArtist())
-                .title(track.getTitle())
-                .version(track.getVersion())
-                .build();
-        trackService.saveTrack(trackAdded);
-    }
-
     @DeleteMapping(value = "/{id}")
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     public void delete(@PathVariable Integer id) {
